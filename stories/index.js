964d7357a471413ca308aef791b362c160b2bffa
JRam2022 <jasonram778@gmail.com>
2022-05-18 19:59:17 -0700

    Create Empty function and stories

--- stories/index.js
+++ stories/index.js
@@ -12,6 +12,7 @@ import InterviewerListItem from "components/InterviewerListItem";
 import InterviewerList from "components/InterviewerList";
 import Appointment from "components/Appointment/index.js";
 import Header from "components/Appointment/Header";
+import Empty from "components/Appointment/Empty";
 
 
 storiesOf("Button", module)
@@ -152,6 +153,11 @@ storiesOf("Button", module)
     .add("Header", () =>(
       <Header
         time={"12pm"}
+      />
+    ))
+    .add("Empty", () => (
+      <Empty
+        onAdd={action("onAdd")}
       />
     ))
 
