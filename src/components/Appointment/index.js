7febe15f909b1ee8d2ee4a8796f113dbdb2e5bb4
JRam2022 <jasonram778@gmail.com>
2022-05-19 14:29:05 -0700

    Create Form with props and stories

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,9 +1,15 @@
 import React from "react";
 import "./styles.scss"
+import Header from "./Header";
+import Empty from "./Empty";
+import Show from "./Show";
 
 
 export default function Appointment(props) {
   return (
-  <arcticle className="appointment"></arcticle>
+  <arcticle className="appointment">
+    <Header time={props.time}/>
+      {props.interview ? <Show interviewer={props.interview.interviewer}student={props.interview.student}/> : <Empty/>}
+  </arcticle>
   );
 }
