221f923dd82de621bbab638d529b3014f2cfe56f
JRam2022 <jasonram778@gmail.com>
2022-05-20 22:45:05 -0700

    Create getInterview function with tests

--- src/Helpers/selectors.js
+++ src/Helpers/selectors.js
@@ -7,3 +7,19 @@ export function getAppointmentsForDay(state, day) {
   // returns list of appointments on that day
   return dayFound.appointments.map((a) => state.appointments[a]);
 }
+
+
+export function getInterview(state, interview) {
+  //checks to see if there is an interview
+  if (!interview) {
+    return interview;
+  }
+  const interviewID = interview.interviewer;
+  const interviewReturnObj = state.interviewers[interviewID];
+  //new object with only student and interviewer info
+  const newState = {
+    student: interview.student,
+    interviewer:interviewReturnObj
+  };
+  return newState;
+}
