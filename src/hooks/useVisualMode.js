ca73a6692727024c8c1a67796f1cb4c5ee71b48c
JRam2022 <jasonram778@gmail.com>
2022-05-21 18:50:36 -0700

    Create check for replace in transition function

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -4,21 +4,27 @@ export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
   const [history, setHistory] = useState([initial]);
 
-  function transition(newMode) {
-    setMode(newMode)
-    setHistory([...history, newMode])
-  }
-  
-  function back() {
-    const secLastIndex = history.length - 2;
-    const setNewHistory = history.slice(0,-1)
+  function transition(newMode, replace = false) {
     
-    if (history.length >= 2) {
-      setMode(history[secLastIndex]);
-      setHistory(setNewHistory);
+    if (replace === true) {
+      console.log(`Transition to ${newMode} by REPLACING ${mode}`);
+      setMode(newMode)
+      setHistory([...history, newMode])
+    } else {
+      setMode(newMode)
+      setHistory([...history])
     }
-    
   }
+
+    function back() {
+      const secLastIndex = history.length - 2;
+      const setNewHistory = history.slice(0,-1)
+      
+      if (history.length >= 2) {
+        setMode(history[secLastIndex]);
+        setHistory(setNewHistory);
+      }
+    }
   
   return { mode, transition, back };
 }
