69a0af6380f4686c453b02d7e5d28bb61a260b00
JRam2022 <jasonram778@gmail.com>
2022-05-25 21:42:22 -0700

    Lint and fix comments

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -3,11 +3,12 @@ import {useState } from "react";
 export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
   const [history, setHistory] = useState([initial]);
-
+  
+  // transitions to new state and updates history
   function transition(newMode, replace = false) {
     
+    // replaces history with the newest state 
     if (replace === true) {
-      console.log(`Transition to ${newMode} by REPLACING ${mode}`);
       setMode(newMode)
       setHistory([...history.slice(0, -1), newMode])
     } else {
@@ -16,10 +17,12 @@ export default function useVisualMode(initial) {
     }
   }
 
+  // goes back to previous history
   function back() {
     const secLastIndex = history.length - 2;
     const setNewHistory = history.slice(0,-1)
     
+    // if history only contains one state it will not revert to empty state
     if (history.length >= 2) {
       setMode(history[secLastIndex]);
       setHistory(setNewHistory);
