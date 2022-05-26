07d91f621ad65163bc7e4fc73227a49c59ec1694
JRam2022 <jasonram778@gmail.com>
2022-05-25 21:37:16 -0700

    Lint and fix spacing

--- src/Helpers/selectors.test.js
+++ src/Helpers/selectors.test.js
@@ -1,6 +1,5 @@
 import { getAppointmentsForDay, getInterview, getInterviewersForDay } from "helpers/selectors";
 
-
 const state = {
   days: [
     {
@@ -72,8 +71,6 @@ test("getAppointmentsForDay returns an empty array when the day is not found", (
   const result = getAppointmentsForDay(state, "Wednesday");
   expect(result.length).toEqual(0);
 });
-////
-
 
 test("getInterviewersForDay returns an array", () => {
   const result = getInterviewersForDay(state, "Monday");
@@ -101,9 +98,6 @@ test("getInterviewersForDay returns an empty array when the day is not found", (
   expect(result.length).toEqual(0);
 });
 
-
-
-///
 test("getInterview returns an object with the interviewer data", () => {
   const result = getInterview(state, state.appointments["3"].interview);
   expect(result).toEqual(
