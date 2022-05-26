af80dfc3942892c580b9fa359972b5aa0e289e2c
JRam2022 <jasonram778@gmail.com>
2022-05-25 21:35:17 -0700

    Lint and fix spacing

--- src/components/__tests__/Form.test.js
+++ src/components/__tests__/Form.test.js
@@ -2,7 +2,6 @@ import React from "react";
 import { render, cleanup, fireEvent } from "@testing-library/react";
 import Form from "components/Form";
 
-
 afterEach(cleanup);
 
 describe("Form", () => {
